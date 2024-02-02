d4b7509415f3fef335a3c996c02f59be3ecd1a87
dnyaneshwar2323 <chavandr23@gmail.com>
2024-02-02 21:20:27 +0530

    first commit

---
+++ src/test.py
@@ -0,0 +1,16 @@
+# mylist=[43, 56, 74, 24, 32, 33, 41]
+# max=mylist[0]
+# for i in mylist:
+#     if i>max:
+#         max=i
+# print(max)
+
+
+def maxnum(mylist):
+    max1=mylist[0]
+    for i in mylist:
+        if i>max:
+            max1=i
+    return max1
+print(max1)
+mylist=[43, 56, 74, 24, 32, 33, 41]
